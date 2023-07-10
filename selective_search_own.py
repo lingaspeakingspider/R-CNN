4103621861f7eb5a7f2bc651d088d139e530453d
oguzkagan <116112769+oguz169kagan@users.noreply.github.com>
2023-07-10 17:31:46 +0300

    Update selective_search_own.py

--- selective_search_own.py
+++ selective_search_own.py
@@ -1,65 +1,91 @@
 import os
 import cv2 as cv
 import skimage
-#import imghdr
+import imghdr
 import selective_search
 
-image="car_IMG_8112.JPG"
+image="car_IMG_8112.jpg"
 src_path=os.getcwd()
 dst_path="C:/Users/Oğuz Kağan/Desktop/destination"
-label_ds="C:/Users/Oğuz Kağan/Desktop/labels"
+label_dst="C:/Users/Oğuz Kağan/Desktop/labels"
 
 #Schreiben Sie den Klassenamen des Fotos an den Anfang des Dokumentnamens. Vergessen Sie das Unterstreichen nicht!
 
-def selective(image, src_path: str , dst_path: str, label_dst: str):
-  valid_image_extensions=['rgb', 'gif', 'pbm', 'pgm', 'ppm', 'tiff', 'rast', 'xbm', 'jpeg', 'jpg', 'bmp', 'png', 'webp', 'exr']
+class Selective_Search:
+   def __init__(self, image, src_path: str , dst_path: str, label_dst):
+      self.image=image
+      self.src_path=src_path
+      self.dst_path=dst_path
+      self.label_dst=label_dst
 
-  if os.path.isdir(src_path)==False or os.path.isdir(label_dst)==False or os.path.isdir(dst_path)==False:
-     raise NotADirectoryError
+   def labeling(self, file, class_idx, x1, x2, y1, y2):
+      os.chdir(label_dst)
+      with open(file, "a") as fp:
+         fp.write("{}   {} {} {} {}\n".format(class_idx, x1, x2, y1, y2))
 
-#   if valid_image_extensions.count(imghdr.what(image))==0:
-#      raise IOError
+   def selective(self):
+      valid_image_extensions=['rgb', 'gif', 'pbm', 'pgm', 'ppm', 'tiff', 'rast', 'xbm', 'jpeg', 'jpg', 'bmp', 'png', 'webp', 'exr']
+      class_names=[]
+      
+      os.chdir(self.src_path)
+      extension=imghdr.what(self.image)
+      
+      if valid_image_extensions.count(extension)==0:
+         raise IOError
 
-  os.chdir(src_path)
-  img = skimage.io.imread(image)
+      if os.path.isdir(self.src_path)==False or os.path.isdir(self.label_dst)==False or os.path.isdir(self.dst_path)==False:
+         raise NotADirectoryError
+      
 
-  boxes = selective_search.selective_search(img, mode='single')
+      #Selective Search
+      os.chdir(self.src_path)
+      img = skimage.io.imread(image)
+      boxes = selective_search.selective_search(img, mode='single')
 
-  # drawing rectangles on the original image
-  img = cv.imread(image)
-  for x1, y1, x2, y2 in boxes:
-      img = cv.rectangle(img, (x1, y1), (x2, y2), color=(0,255,0), thickness=1)
+      #Labels
+      os.chdir(self.label_dst)
+      liste=os.listdir(self.label_dst)
+      if liste.count("classes.txt")==0:
+         with open("classes.txt", "w") as f:
+            pass
+         
+      list1=image.split("_")
+      class_name=list1[0]
+      class_names.append(class_name)
+      
+      if class_names.count(class_names)==0:
+         with open("classes.txt", "a") as file:
+            file.write(class_name + "\n")
+         
+      with open("classes.txt", "r") as f:
+         list_oguz=f.readlines()
 
-  #Labels
-  os.chdir(label_dst)
-  liste=os.listdir(label_dst)
-  if liste.count("classes.txt")==0:
-     with open("classes.txt", "w") as f:
-        pass
-     
-  list1=image.split("_")
-  class_name=list1[0]
+      length=len(list_oguz)-1
+      for i in list_oguz:
+         if i!=length:
+            index=list_oguz.index(i)
+            list_oguz[index]=list_oguz[index].replace("\n", "")
 
-  with open("classes.txt", "a") as file:
-     file.write(class_name + "\n")
-     
-  with open("classes.txt", "r") as f:
-    list_oguz=f.readlines()
+      class_idx=list_oguz.index(class_name)
 
-  length=len(list_oguz)-1
-  for i in list_oguz:
-    if i!=length:
-        index=list_oguz.index(i)
-        list_oguz[index]=list_oguz[index].replace("\n", "")
+      #Drawing bounding boxes
+      os.chdir(self.src_path)
+      img = cv.imread(image)
+      with open(self.image + ".txt", "w") as f:
+            pass
+      for x1, y1, x2, y2 in boxes:
+         Selective_Search.labeling(self, file=self.image + ".txt", class_idx=class_idx, x1=x1, x2=x2, y1=y1,y2=y2)
+         img = cv.rectangle(img, (x1, y1), (x2, y2), color=(0,255,0), thickness=1)
 
-  class_idx=list_oguz.index(class_name)
+      #Sending the image to destination path
+      os.chdir(self.dst_path)
+      cv.imwrite(image + "(selective).png", img)
 
-  with open(image + ".txt", "w") as fp:
-     fp.write("{}   {} {} {} {}".format(class_idx, x1, x2, y1, y2))
+      #Controlling
+      src_list=os.listdir(self.src_path)
+      if src_list.count(image + ".txt")!=0:
+         os.remove(os.path.join(self.src_path, image + ".txt"))
 
-  #Printing the image
-  os.chdir(dst_path)
-  cv.imwrite(image + "(selective).png", img)
 
-selective(image=image, src_path=src_path, dst_path=dst_path, label_dst=label_ds)
-
+model=Selective_Search(image=image, src_path=src_path, dst_path=dst_path, label_dst=label_dst)
+model.selective()
